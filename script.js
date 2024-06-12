3df40aedfa459df61513be4f4606911ff1edfdba
cr <72098121+CyberRodeo@users.noreply.github.com>
2024-06-13 02:50:53 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,40 +1,68 @@
-const actors = [
-  {
-    "name" : "Eddie",
-    "age" : 25, 
-    "email" : "eddie@st.com",
-    "id" : "1jk1ui3hy2i1u3"
-  },
-  {
-    "name" : "Max",
-    "age" : 19,
-    "email" : "max@st.com",
-    "id" : "12j3b1hk3v2"
-  }
+// This is what our customer data looks like.
+const customerData = [
+  { ssn: "444-44-4444", name: "Bill", age: 35, email: "bill@company.com" },
+  { ssn: "555-55-5555", name: "Donna", age: 32, email: "donna@home.org" },
 ];
 
-var db;
-var request = window.indexedDB.open("MyFamily");
 
-request.onerror = event => {
-  console.error("Database error: " + event.target.errorCode);
-};
-request.onsuccess = event => {
-  db = event.target.result;
-};
+const dbName = "the_name";
 
-const makeObjectStore = () => {
-   const objectStore = request.createObjectStore(["actors", { keyPath: "id" }]);
+const request = indexedDB.open(dbName, 2);
 
-   objectStore.createIndex("name", "name", { unique : false });
-   objectStore.createIndex("email", "email", { unique : true });
-
-   const transaction = objectStore.transaction("actors", "readwrite");
-   transaction.oncomplete = () => {
-      actors.forEach((actor) => {
-        transaction.objectStore("actors").add(actor);
-      });
-   };
+request.onerror = (event) => {
+  // Handle errors.
 };
 
-makeObjectStore();
+request.onsuccess = (event) => {
+  const db = event.target.result;
+
+  const transaction = db.transaction(["customers"], "readwrite");
+  transaction.oncomplete = () => {
+    console.log("transaction complete!");
+  };
+
+  transaction.onerror = () =>{
+    console.log("Transaction could not be completed!");
+  };
+
+  const request = transaction.objectStore("customers");
+  request.add({
+    
+      "name" : "El",
+      "email" : "el@st.com",
+      "age" : "17",
+      "ssn" : "999-99-999"
+    
+  })
+};
+
+
+request.onupgradeneeded = (event) => {
+  const db = event.target.result;
+
+  // Create an objectStore to hold information about our customers. We're
+  // going to use "ssn" as our key path because it's guaranteed to be
+  // unique - or at least that's what I was told during the kickoff meeting.
+  const objectStore = db.createObjectStore("customers", { keyPath: "ssn" });
+
+  // Create an index to search customers by name. We may have duplicates
+  // so we can't use a unique index.
+  objectStore.createIndex("name", "name", { unique: false });
+
+  // Create an index to search customers by email. We want to ensure that
+  // no two customers have the same email, so use a unique index.
+  objectStore.createIndex("email", "email", { unique: true });
+
+  // Use transaction oncomplete to make sure the objectStore creation is
+  // finished before adding data into it.
+  objectStore.transaction.oncomplete = (event) => {
+    // Store values in the newly created objectStore.
+    const customerObjectStore = db
+      .transaction("customers", "readwrite")
+      .objectStore("customers");
+    customerData.forEach((customer) => {
+      customerObjectStore.add(customer);
+    });
+  };
+};
+
