7cbb7f6c99fdaca003fe140403c7d1e74f0332fb
cr <72098121+CyberRodeo@users.noreply.github.com>
2024-06-13 18:36:42 +0530

    indexedDB API all instance working

--- script.js
+++ script.js
@@ -1,68 +1,70 @@
-// This is what our customer data looks like.
-const customerData = [
-  { ssn: "444-44-4444", name: "Bill", age: 35, email: "bill@company.com" },
-  { ssn: "555-55-5555", name: "Donna", age: 32, email: "donna@home.org" },
-];
+window.onload = () => {
+  
+  const songs = [
+    {
+      "name" : "Faded",
+      "artist" : "Alan Walker",
+      "id" : "hu1bh2ubj2o1ej2"
+    },
+    {
+      "name" : "Alone",
+      "artist" : "Alan Walker",
+      "id" : "niajhwuo2huo2h"
+    }
+  ];
+ // Hold an instance of a db object for us to store the IndexedDB data in
+  let db;
 
+  // Let us open our database
+  const DBOpenRequest = window.indexedDB.open('songs', 3);
 
-const dbName = "the_name";
-
-const request = indexedDB.open(dbName, 2);
-
-request.onerror = (event) => {
-  // Handle errors.
-};
-
-request.onsuccess = (event) => {
-  const db = event.target.result;
-
-  const transaction = db.transaction(["customers"], "readwrite");
-  transaction.oncomplete = () => {
-    console.log("transaction complete!");
+  // Register two event handlers to act on the database being opened successfully, or not
+  DBOpenRequest.onerror = (event) => {
+    console.log("db cant init");
   };
 
-  transaction.onerror = () =>{
-    console.log("Transaction could not be completed!");
+  DBOpenRequest.onsuccess = (event) => {
+    console.log("DB init");
+    db = DBOpenRequest.result;
+
+    const transaction = db.transaction("songs", "readwrite").objectStore("songs");
+    transaction.oncomplete = () =>{
+      console.log("tran complete");
+    };
+
+    transaction.onerror = () => {
+      console.log("tran imcomplete");
+    };
+
+    const lolz = transaction.get("hu1bh2ubj2o1ej2");
+
+    lolz.onsuccess = () => {
+      console.log(lolz.result);
+    };
+
+    lolz.onerror = () => {
+      console.log("fuck u ");
+    };
   };
 
-  const request = transaction.objectStore("customers");
-  request.add({
+  DBOpenRequest.onupgradeneeded = (event) => {
+    db = event.target.result;
+
+    db.onerror = (event) => {
+      console.log("error loading database");
+    };
+
+
+    // Create an objectStore for this database
+    const objectStore = db.createObjectStore(["songs"], { keyPath: "id"});
+    objectStore.createIndex("name", "name", { unique: false});
+    objectStore.createIndex("id", "id", { unique : true});
+
+    objectStore.transaction.oncomplete = (event) => {
+      // Store values in the newly created objectStore.
+      console.log("complete");
+    };
+
     
-      "name" : "El",
-      "email" : "el@st.com",
-      "age" : "17",
-      "ssn" : "999-99-999"
-    
-  })
+  };  
 };
-
-
-request.onupgradeneeded = (event) => {
-  const db = event.target.result;
-
-  // Create an objectStore to hold information about our customers. We're
-  // going to use "ssn" as our key path because it's guaranteed to be
-  // unique - or at least that's what I was told during the kickoff meeting.
-  const objectStore = db.createObjectStore("customers", { keyPath: "ssn" });
-
-  // Create an index to search customers by name. We may have duplicates
-  // so we can't use a unique index.
-  objectStore.createIndex("name", "name", { unique: false });
-
-  // Create an index to search customers by email. We want to ensure that
-  // no two customers have the same email, so use a unique index.
-  objectStore.createIndex("email", "email", { unique: true });
-
-  // Use transaction oncomplete to make sure the objectStore creation is
-  // finished before adding data into it.
-  objectStore.transaction.oncomplete = (event) => {
-    // Store values in the newly created objectStore.
-    const customerObjectStore = db
-      .transaction("customers", "readwrite")
-      .objectStore("customers");
-    customerData.forEach((customer) => {
-      customerObjectStore.add(customer);
-    });
-  };
-};
-
